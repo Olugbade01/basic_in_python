f5988a5d0c64e9da6e17f60472a9289d415aeaf6
ionifade <idowuonifade2@gmail.com>
2026-07-20 09:26:37 +0100

    Day2

---
+++ day1/main.py
@@ -0,0 +1,24 @@
+print("Hello people") # This prints the content as a strings data type
+
+print(3) #this prints the int 
+
+print(3>4)  #This prints the value of the boolean 
+
+print('true') # this works like an alternative for printing text as string
+
+#arithemetics 
+
+print(4/2) # this print as a float 
+
+print(4+2) #This prints as int
+
+print(4-2) #This prints as int
+
+
+print(4*2)#This prints as int
+print(4**3) #This result the exponential as an int
+
+print(4%2)#This is the modulus 
+
+print(8//2)#This prints only the numbers of times the division can be done
+
---
+++ day2/main.py
@@ -0,0 +1,46 @@
+compN = complex(2, 3)
+
+print(compN)
+
+false = bool(0)
+print(false)
+
+
+char = chr(56)# This print the character in the index 56 on the ascii table
+print(type(char))
+
+maxim = min(3,47,34)# this print the minimum values of the elements 
+print(oct(300))# this print the octal values as strings
+print(maxim)
+print(char)
+print(type(format(3.1967, '.2f'))) # this correct it to 2dp as strings data type 
+print(type(round(3.1967, 2))) # this can equivalent to the above line but in float data type
+power = pow(4,2) 
+print(power)
+name = input("what is your name? ") # this request an argument from the CLI 
+
+print(name)
+
+
+
+
+# Ass
+
+firstname, lastname = "Abubakr", "Akeeb"
+fullName = firstname + " " + lastname
+country_name = "Nigeria"
+city = "Iwo"
+age = 27
+year = 2026
+is_married = True
+is_true = True
+is_light_on = "yes"
+
+print(type(firstname))
+print(type(lastname))
+print(type(city))
+print(type(is_married))
+print(type(year))
+print(type(is_married))
+
+lenFistNAme = len(firstname)
