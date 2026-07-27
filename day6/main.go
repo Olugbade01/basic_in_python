17655304b292093a643baa018a8128ec63f86132
ionifade <idowuonifade2@gmail.com>
2026-07-27 09:23:51 +0100

    day6

---
+++ day6/main.go
@@ -0,0 +1,8 @@
+package main 
+
+import "fmt"
+
+func main(){
+	items := ("item1" "item2", "item3", "item4", "item5", 100)
+	fmt.Println(items)
+}
---
+++ day6/main.py
@@ -0,0 +1,8 @@
+tup = tuple() #empty tuple
+
+fruits = ('mango', 'watermelon', 'apples', 'orange', 'banana')
+# print(fruits.__class__())
+fruits2 = ('agbalumo', 'osan', 'ogede')
+# print(fruits.__add__(fruits2))
+# print(fruits.index('mango'))
+print(fruits.)
