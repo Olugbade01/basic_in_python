dfcdd2e079860b72c94b7c42737d0c6a5a404ac9
aakeeb <abubakarakeeb07031@gmail.com>
2026-07-27 11:47:38 +0100

    Updaed

---
+++ day6/ass.py
@@ -0,0 +1,28 @@
+# emptuple = ()
+# brothers = ('Dare', 'Soji', 'Ridwan', 'Ibrahim', 'Abdul-Salaam')
+# sisters = ('Bola', 'Shade', 'Tolani', 'Aisha')
+# siblings = brothers.__add__(sisters)
+# print(siblings)
+
+# print(len(siblings))
+# family_members = ('Badmus', 'Awero').__add__(siblings)
+# print(family_members)
+
+# # Excercise 2 
+# father, mother, *sibling = family_members
+# print(sibling)
+# print(father)
+# fruits = ('mango', 'grapes', 'banana', 'watermelon', 'orange')
+# vegetables = ('lettles', 'waterleaf', 'cucumber', 'tomatoes')
+# animal_product = ('meat', 'animal-skin', 'fur-coat', 'calcium')
+# food_stuff_tp = fruits.__add__(vegetables).__add__(animal_product)
+# print(food_stuff_tp)
+# food_stuff_lt = list(food_stuff_tp)
+# print(food_stuff_lt)
+# print(food_stuff_tp[int(len(food_stuff_tp)/2)])
+# print(food_stuff_lt[:3], food_stuff_lt[int(len(food_stuff_lt)-3):])
+# del food_stuff_tp
+
+# nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+# print(nordic_countries.__contains__('Estonia'))
+# print(nordic_countries.__contains__('Iceland'))
