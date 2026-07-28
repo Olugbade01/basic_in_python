a3cfe8edb4d76e6f9b5262d5d919eb2c11c957d4
aakeeb <abubakarakeeb07031@gmail.com>
2026-07-28 17:33:57 +0100

    Updated

---
+++ day9/ass.py
@@ -0,0 +1,42 @@
+# # Excercise Level 1 question 3
+
+# a = int(input('Enter a number here: '))
+# b = int(input('Enter a second number: '))
+
+# if a > b: 
+#     print('a is greater than b')
+# elif a < b: 
+#     print('a is smaller than b')
+# else:
+#     print('a is equal to b')
+
+# # Excercise 2: level 2
+# # 2.1
+
+# score = int(input('Enter your score here: '))
+
+# if 90 <= score <= 100:
+#     print('Your grade is A')
+# elif 80 <= score < 90:
+#     print('Your grade is B')
+# elif 70 <= score < 80:
+#     print('Your grade is C')
+# elif 60 <= score < 70:
+#     print('Your grade D')
+# else: 
+#     print('Your grade is F')
+
+# # 2.2
+# month = input('Enter the month you are! ')
+# if month == 'September' or month == 'October' or month == 'December':
+#     print('Its Autummn season out there')
+# elif month == 'December' or month == 'January' or month == 'February':
+#     print('Its Winter season out there!')
+# elif month == 'March' or month == 'April' or month == 'May':
+#     print('Its the Spring season out there!')
+# elif month == 'June' or month == 'July' or month == 'Auguest':
+#     print('Its Summer season out there!')
+# else: 
+#     print('Enter a valid month')
+
+# 2.3 
