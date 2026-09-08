77574765d16d9d3c011b636ca87978381bfe2ca9
aakeeb <abubakarakeeb07031@gmail.com>
2026-09-08 10:46:05 +0100

    UPDATED

--- day_13/ass.py
+++ day_13/ass.py
@@ -1,3 +1,24 @@
+# # Exercise Level 1
+
+# # 1.1
+# numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+
+# neg_and_zero = [num for num in numbers if num <= 0]
+# print(neg_and_zero)
+
+# # 1.2
+
+
+# list_of_lists =[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# single_list_of_list = [number for numb in list_of_lists for number in numb]
+# print(single_list_of_list)
+
+# # 1.3
+# list_of_tupes = [(i, 1, i *1,i **2,i ** 3, i **4, i**5)for i in range(11)]
+
+# print(list_of_tupes)
+
 # # 1.4
 
 # countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
@@ -18,5 +39,3 @@
 
 # result = [list_names for name in names for list_names in name]
 # print(result)
-
-
